c8d69aa304888ae0434be8afbe6d70cec549a930
Edward Cole Hajek <edwardcole2001@gmail.com>
2022-09-20 20:05:17 +0000

    added my major to the output

---
+++ hello.c
@@ -0,0 +1,19 @@
+/**
+ * Author: Edward Cole Hajek & John M
+ *         chajek@unomaha.edu & jmentgen@unomaha.edu
+ * Date: 2022/08/23
+ *
+ * A simple hello world program in C for the 5th time for me!
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("\nHello World!"); /** Please let me die oh god please why!**/
+  printf("\nThis file was written by Cole Hajek and it is very late!!! (ahhhhhh!!)\n\n");
+  printf("additionally I am majoring in electrical engineering\n\n");
+
+  return 0;
+}
